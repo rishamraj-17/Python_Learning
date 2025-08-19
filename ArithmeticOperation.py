0cfa157d785d66c6029b2749dd96923ccc1601e9
risham <rajrisham06@gmail.com>
2025-08-19 21:22:19 +0545

    arithmetic operation basic

---
+++ ArithmeticOperation.py
@@ -0,0 +1,9 @@
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+#also Note this
+print(10 // 3) #int value of division
+print(10 % 3)
+#Good luck guessing this operator
+print(10 ** 3) #its power 10^3  
